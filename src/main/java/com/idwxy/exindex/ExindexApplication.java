86247542ea56e9b685f5e2cc32d3a155fc8ce54f
idwxy <idwxy@live.com>
2019-09-10 23:02:57 +0800

    first commit

---
+++ src/main/java/com/idwxy/exindex/ExindexApplication.java
@@ -0,0 +1,13 @@
+package com.idwxy.exindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExindexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExindexApplication.class, args);
+	}
+
+}
