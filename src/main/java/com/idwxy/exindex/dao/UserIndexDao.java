1925c9b1f850f5e199399c478a736e64b6b8538c
idwxy <idwxy@live.com>
2019-09-11 23:29:16 +0800

    增加用户指标数据 Dao 层

---
+++ src/main/java/com/idwxy/exindex/dao/UserIndexDao.java
@@ -0,0 +1,36 @@
+package com.idwxy.exindex.dao;
+
+import com.idwxy.exindex.entity.UserIndex;
+
+import java.util.List;
+
+public interface UserIndexDao {
+
+    /**
+     * 通过用户 id 和指标类型获取用户的所有指标
+     * @param userIndex
+     * @return
+     */
+    List<UserIndex> getById(UserIndex userIndex);
+
+    /**
+     * 创建用户生理指标数据
+     * @param userIndex
+     * @return
+     */
+    int insert(UserIndex userIndex);
+
+    /**
+     * 更新用户生理指标数据
+     * @param userIndex
+     * @return
+     */
+    int update(UserIndex userIndex);
+
+    /**
+     * 删除用户生理指标数据
+     * @param userIndexId
+     * @return
+     */
+    int delete(Integer userIndexId);
+}
