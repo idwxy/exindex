853930a8a2d899924fa7b7b591d1e6a7adc34f74
idwxy <idwxy@live.com>
2019-09-12 22:04:24 +0800

    增加用户控制层

---
+++ src/main/java/com/idwxy/exindex/controller/UserController.java
@@ -0,0 +1,66 @@
+package com.idwxy.exindex.controller;
+
+import com.idwxy.exindex.common.PaginationObject;
+import com.idwxy.exindex.common.ResultObject;
+import com.idwxy.exindex.entity.User;
+import com.idwxy.exindex.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // 创建用户
+    @PostMapping("/add")
+    public ResultObject insertUser(@RequestBody User user) {
+        int modifyId = userService.insertUser(user);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("modifyId", modifyId);
+        ResultObject resultObject = new ResultObject(200, " ", map);
+        return resultObject;
+    }
+
+    // 查询用户
+    @GetMapping("/get")
+    public ResultObject getUserById(@RequestParam Integer id) {
+        User user = userService.getUserById(id);
+        ResultObject resultObject = new ResultObject(200, "", user);
+        return resultObject;
+    }
+
+    // 分页查询用户
+    @GetMapping("/list")
+    public ResultObject getAllUser(@RequestParam Integer pageNum, @RequestParam Integer pageSize) {
+        PaginationObject paginationObject = userService.getAllUser(pageNum, pageSize);
+        ResultObject resultObject = new ResultObject(200, "", paginationObject);
+        return resultObject;
+    }
+
+    // 删除用户
+    @GetMapping("/delete")
+    public ResultObject delete(@RequestParam Integer id) {
+        int modifyId = userService.delete(id);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("modifyId", modifyId);
+        ResultObject resultObject = new ResultObject(200, "", map);
+        return resultObject;
+    }
+
+    // 更新用户
+    @PutMapping("/edit")
+    public ResultObject updateUser(@RequestBody User user) {
+        int modifyId = userService.updateUser(user);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("modifyId", modifyId);
+        ResultObject resultObject = new ResultObject(200, "", map);
+        return resultObject;
+    }
+
+}
