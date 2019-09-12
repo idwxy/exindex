56acf2984098590910d966b1563ba68ea0299fa2
idwxy <idwxy@live.com>
2019-09-12 22:35:30 +0800

    程序入口注解入 dao 层

--- src/main/java/com/idwxy/exindex/ExindexApplication.java
+++ src/main/java/com/idwxy/exindex/ExindexApplication.java
@@ -1,9 +1,11 @@
 package com.idwxy.exindex;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.idwxy.exindex.dao")
 public class ExindexApplication {
 
 	public static void main(String[] args) {
